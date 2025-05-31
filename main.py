96d2aee28c793f91ce5184916e3b170e7afe5696
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-05-31 17:33:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hoang Ngoc Lam LE",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': "mailto:lammin231@gmail.com",
+        'Report a bug': "mailto:lammin231@gmail.com",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+accueil_page = st.Page("accueil.py", title="Accueil", icon=":material/add_circle:")
+personel_page = st.Page("presentation.py", title="Who am I", icon=":material/delete:")
+analyse_dt_page = st.Page("app_multi/analyse_dt.py", title="Analyse Dataframe", icon=":material/dashboard:")
+test_page = st.Page("app_multi/test.py", title="Analyse Dataframe", icon=":material/delete:")
+
+pg = st.navigation(
+    {
+        "Home" : [accueil_page, personel_page], 
+        "Application" :[analyse_dt_page, test_page]
+    }
+)
+pg.run()
