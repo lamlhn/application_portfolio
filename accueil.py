2cb27838ef4e56f957153a1f49928df78ef684e1
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-05-31 17:34:16 +0200

    Add files via upload

---
+++ accueil.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: #6C3483;'>Bienvenue sur mon portfolio - Guide d'utilisation du site</h1>", unsafe_allow_html=True)
+
+st.markdown("""
+Bienvenue sur mon site personnel ! 🌟  
+Ce site vous permet de découvrir mon profil et d'explorer des données de manière interactive.
+
+### 📌 Navigation :
+- **Présentation** : découvrez qui je suis.
+- **Interaction avec les données** : explorez, recherchez, et visualisez des informations.
+
+👉 Utilisez le menu à gauche pour commencer la visite !
+""")
+
+st.markdown("---")
+
+# === Footer ou suite ===
+st.markdown("""
+<div style='text-align: center; font-size:16px;'>
+    💬 N'hésitez pas à me contacter pour toute opportunité d'alternance !<br><br>
+    📞 07 48 15 66 21 &nbsp;&nbsp;|&nbsp;&nbsp; 📧 lammin231@gmail.com &nbsp;&nbsp;|&nbsp;&nbsp; 📍 Cachan (94230)
+</div>
+""", unsafe_allow_html=True)
