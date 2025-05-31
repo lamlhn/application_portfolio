b71d354631d44da9113f8f3ea6c77b44f12dfa3b
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-05-31 22:22:56 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from openai import OpenAI
+import openai
 import os
 
 st.markdown("<h1 style='text-align: center; color: #6C3483;'>Votre Assistant IA</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Posez-moi vos questions sur les études internationales, les coûts ou les programmes !</p>", unsafe_allow_html=True)
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+openai.api_key=st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -20,18 +20,22 @@ for message in st.session_state.message:
 prompt = st.chat_input("Posez votre question")
 
 if prompt:
-    st.session_state.message.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+    st.session_state.message.append({"role": "user", "content": prompt})
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model = st.st.session_state["openai_model"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
             stream=True,
-        )
-        response = st.write_stream(stream)
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "| ")
+        message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": response})
