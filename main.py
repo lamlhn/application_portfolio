e9f7a6d105038359c24bfe2e7bb2d3e1fd77f07c
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-07-13 22:38:06 +0200

    upload new feature

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ accueil_page = st.Page("accueil.py", title="Accueil", icon=":material/home:")
 personel_page = st.Page("presentation.py", title="Who am I", icon=":material/person:")
 analyse_dt_page = st.Page("analyse_dt.py", title="Analyse Dataframe", icon=":material/dashboard:")
 chatbot_page = st.Page("chatbot.py", title="Chatbot", icon=":material/support_agent:")
-
+page_predict = st.Page("page_predict.py", title="Smart Predict", icon=":material/dashboard:")
 pg = st.navigation(
     {
         "Home" : [accueil_page, personel_page], 
-        "Application" :[analyse_dt_page, chatbot_page]
+        "Application" :[analyse_dt_page, chatbot_page, page_predict]
     }
 )
+
 pg.run()
