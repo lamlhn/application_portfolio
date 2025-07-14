e35854e149eedb08509550c8f58501ef6d27fc3b
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-07-14 10:20:14 +0200

    hide chatbot

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ st.set_page_config(
 accueil_page = st.Page("accueil.py", title="Accueil", icon=":material/home:")
 personel_page = st.Page("presentation.py", title="Who am I", icon=":material/person:")
 analyse_dt_page = st.Page("analyse_dt.py", title="Analyse Dataframe", icon=":material/dashboard:")
-chatbot_page = st.Page("chatbot.py", title="Chatbot", icon=":material/support_agent:")
+# chatbot_page = st.Page("chatbot.py", title="Chatbot", icon=":material/support_agent:")
 page_predict = st.Page("page_predict.py", title="Smart Predict", icon=":material/dashboard:")
 pg = st.navigation(
     {
         "Home" : [accueil_page, personel_page], 
-        "Application" :[analyse_dt_page, chatbot_page, page_predict]
+        # "Application" :[analyse_dt_page, chatbot_page, page_predict]
+        "Application" :[analyse_dt_page, page_predict]
     }
 )
 
