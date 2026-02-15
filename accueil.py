d252a0a44b7569e54587c80181daf2556f11ee11
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2026-02-15 14:37:32 +0100

    Update accueil.py

--- accueil.py
+++ accueil.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: #6C3483;'>Bienvenue sur mon portfolio - Guide d'utilisation du site</h1>", unsafe_allow_html=True)
 
 st.markdown("""
-Bienvenue sur mon site personnel ! 🌟  
+Bienvenue sur mon site personnel !   
 Ce site vous permet de découvrir mon profil et d'explorer des données de manière interactive.
 
-### 📌 Navigation :
+### Navigation :
 - **Présentation** : découvrez qui je suis.
 - **Interaction avec les données** : explorez, recherchez, et visualisez des informations.
 
-👉 Utilisez le menu à gauche pour commencer la visite !
+Utilisez le menu à gauche pour commencer la visite !
 """)
 
 st.markdown("---")
@@ -18,8 +18,8 @@ st.markdown("---")
 # === Footer ou suite ===
 st.markdown("""
 <div style='text-align: center; font-size:16px;'>
-    💬 N'hésitez pas à me contacter pour toute opportunité d'alternance !<br><br>
-    📞 07 48 15 66 21 &nbsp;&nbsp;|&nbsp;&nbsp; 📧 lammin231@gmail.com &nbsp;&nbsp;|&nbsp;&nbsp; 📍 Cachan (94230)
+    N'hésitez pas à me contacter pour toute opportunité d'alternance !<br><br>
+    Tel: 07 48 15 66 21 &nbsp;&nbsp;|&nbsp;&nbsp; Mail: lammin231@gmail.com &nbsp;&nbsp;|&nbsp;&nbsp; Adresse: Roubaix 59100
 </div>
 """, unsafe_allow_html=True)
 
